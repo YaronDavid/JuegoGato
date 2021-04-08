10ed82b9158a016de01a4d005e484fc5a2a982c2
YaronDavid <43763605+YaronDavid@users.noreply.github.com>
2021-04-08 17:57:17 -0500

    Update JuegoGato.java

--- JuegoGato.java
+++ JuegoGato.java
@@ -1,36 +1,44 @@
 
+
 package juegogato;
 import java.io.*;
 import java.util.Scanner;
 
 public class JuegoGato {
-    PipedInputStream in[];
-    PipedOutputStream out[];
-    hilos h[];
-    public void main(String[] args) {
+    private static void inicio() {
+        PipedInputStream in[];
+        PipedOutputStream out[];
+        hilos h[];
         String numero;
         int valor;
+        int random = 0;
         System.out.println("Ingresa el numero de partidas: ");
         Scanner teclado=new Scanner(System.in);
         numero=teclado.nextLine();
         valor=Integer.parseInt(numero);
+        out=new PipedOutputStream[valor*2];
+        in=new PipedInputStream[valor*2];
+        h=new hilos[valor*2];
         int i;
         for(i=0; i<valor*2; i++){
+            random=(int) ((Math.random()) * 20)%2;
             try{
-            out[i]=new PipedOutputStream();
-            in[i]=new PipedInputStream(out[i]);
-            for(i=0; i<valor*2; i++){
+                out[i]=new PipedOutputStream();
+                in[i]=new PipedInputStream(out[i]);
+            }catch(IOException e){}
+        }
+        for(i=0; i<valor*2; i++){
+            try{
                 if(i%2==0){
-                h[i]=new hilos(in[i], out[i+1]);
-            }
-            else
-                h[i]=new hilos(in[i], out[i-1]);
-            }
-            for(i=0; i<valor*2; i++){
-                h[i].start();
-            }
+                    h[i]=new hilos(in[i], out[i+1], random, 1);//Tablero de Circulo
+                }
+                else
+                    h[i]=new hilos(in[i], out[i-1], random, 0);//Tablero de Tache asociado al Circulo
             }catch(Exception e){}
         }
     }
-    
+    public static void main(String args[]){
+        inicio();
+    }
 }
+
